aeae491f3b13cc167a666bf5095ee6f47fd7c31b
Chris Pappas <christina.pappas@uky.edu>
2017-11-21 13:56:22 -0500

    sampled serpent data, created tin

--- effigy-tin-server/effigy-sample.js
+++ effigy-tin-server/effigy-sample.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const turf = require('@turf/turf')
 
 // read the points GeoJSON file
-fs.readFile('data/serpent.json', 'utf8', (err, data) => {
+fs.readFile('serpent.json', 'utf8', (err, data) => {
     if (err) throw err
     // parse the string to GeoJSON
     const points = JSON.parse(data)
@@ -10,7 +10,7 @@ fs.readFile('data/serpent.json', 'utf8', (err, data) => {
     const sample = turf.sample(points, 100)
 
     // stringify the GeoJSON and write to file
-    fs.writeFile('data/serpent-sample.json', JSON.stringify(sample), 'utf8', (err) => {
+    fs.writeFile('serpent-sample.json', JSON.stringify(sample), 'utf8', (err) => {
         if (err) throw err
         console.log('serpent-sample.json written to file')
     });
--- effigy-tin-server/effigy-tin.js
+++ effigy-tin-server/effigy-tin.js
@@ -2,15 +2,15 @@ const fs = require('fs')
 const turf = require('@turf/turf')
 
 // read the points GeoJSON file
-fs.readFile('data/serpent.json', 'utf8', (err, data) => {
+fs.readFile('serpent-sample.json', 'utf8', (err, data) => {
     if (err) throw err
     // parse the string to GeoJSON
     const points = JSON.parse(data)
     // convert to a TIN GeoJSON
-    const tin = turf.tin(points)
+    const tin = turf.tin(points, 'Z')
 
     // stringify the GeoJSON and write to file
-    fs.writeFile('data/serpent-tin.json', JSON.stringify(tin), 'utf8', (err) => {
+    fs.writeFile('serpent-tin.json', JSON.stringify(tin), 'utf8', (err) => {
         if (err) throw err
         console.log('serpent-tin.json written to file')
     });
