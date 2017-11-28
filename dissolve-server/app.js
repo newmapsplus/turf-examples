83d40d2730dd225f61c05102dc2c455e8da71c5d
Rich Donohue <rgdonohue@gmail.com>
2017-11-28 16:32:35 -0700

    edit dissolve-server

--- dissolve-server/app.js
+++ dissolve-server/app.js
@@ -4,15 +4,20 @@ const turf = require('@turf/turf')
 // read the points GeoJSON file
 fs.readFile('buffers.json', 'utf8', (err, data) => {
     if (err) throw err
+    
     // parse the string to GeoJSON
-    const buffers = JSON.parse(data)
-    // dissolves features
-    const dissolve = turf.dissolve(buffers)
+    let buffers = JSON.parse(data)
+
+    // keep iterating until there is only one feature
+    while(buffers.features.length > 1) {
+        buffers = turf.dissolve(buffers)
+    }
+    
     // output file name
     const outFileName = 'dissolve.json'
     
-    // stringify the GeoJSON and write to file
-    fs.writeFile(outFileName, JSON.stringify(dissolve), 'utf8', (err) => {
+    // // stringify the GeoJSON and write to file
+    fs.writeFile(outFileName, JSON.stringify(buffers), 'utf8', (err) => {
         if (err) throw err
         console.log(outFileName + ' written to file')
     });
